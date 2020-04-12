ddada1b3fa7eed10b21a63428d08524651d0a024
dscrane <daegancrane@gmail.com>
2020-04-12 10:26:38 -0400

    init

---
+++ build/precache-manifest.cab73dd00c6e9d627b17049676414227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da3c0540be334b927863dab3d477fb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe71167078457dbc0cb5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3c9052ce081c9d1c3f25",
+    "url": "/static/css/main.54b911e8.chunk.css"
+  },
+  {
+    "revision": "fe71167078457dbc0cb5",
+    "url": "/static/js/2.8de680e8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.8de680e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9052ce081c9d1c3f25",
+    "url": "/static/js/main.e2f1b5ef.chunk.js"
+  },
+  {
+    "revision": "8a9293cb1f6725a8cbd9",
+    "url": "/static/js/runtime-main.8af293b8.js"
+  }
+]);
